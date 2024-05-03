5c58e0af2190cf127abc16aa8698bb2b065fafc7
mvrcoag <dev.nnqp2@slmails.com>
2024-05-03 15:44:53 -0600

    feat: :sparkles: user domain

---
+++ User/domain/User.ts
@@ -0,0 +1,27 @@
+import { UserCreatedAt } from "./UserCreatedAt";
+import { UserEmail } from "./UserEmail";
+import { UserId } from "./UserId";
+import { UserName } from "./UserName";
+
+export class User {
+  id: UserId;
+  name: UserName;
+  email: UserEmail;
+  createdAt: UserCreatedAt;
+
+  constructor(
+    id: UserId,
+    name: UserName,
+    email: UserEmail,
+    createdAt: UserCreatedAt
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.createdAt = createdAt;
+  }
+
+  public nameAndEmail() {
+    return `${this.name} - ${this.email}`;
+  }
+}
